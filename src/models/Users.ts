62ed4ca952dc7da23121ceaae53bf014f5466ad2
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-12 14:45:05 +0330

    created UserModel

---
+++ src/models/Users.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    label:{
+        type:String,
+        trim:true,
+    },
+    street:{
+        type:String,
+        trim: true,
+    },
+    city:{
+        type:String,
+        trim:true,
+    },
+    state:{
+        type:String,
+        trim:true,
+    },
+    zipCode:{
+        type:String,
+        trim:true,
+    },
+    country:{
+        type:String,
+        trim:true,
+    }
+});
+
+const userSchema = new mongoose.Schema({
+    firstName:{type:String , required:true , trim:true},
+    lastName:{type:String , required:true , trim:true},
+    email:{type:String , required:true , trim:true},
+    password:{type:String , required:true , trim:true},
+    role:{type:String , required:true , trim:true},
+    phone:{type:String , enum:['ADMIN','CUSTOMER'], default:"CUSTOMER", required:true},
+    profileImage:{type:String},
+    dateOfBirth:{type:Date},
+    addresses:[addressSchema],
+    isActive:{type:Boolean , default:true},
+    gender:{type:String , enum:['male', 'female' ,'other']}
+},{
+    timestamps:true
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+export default UserModel
