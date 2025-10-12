ce84fddf25c3f260fc1d1d08bf288bb675425e8f
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-12 16:50:58 +0330

    created authController

---
+++ src/controller/auth.controller.ts
@@ -0,0 +1,22 @@
+import { authService } from "../services/authServices";
+import {req , res} from '../types/express'
+
+export const authController = {
+    async register(req:req ,  res:res){
+        try{
+            const data = req.body;
+            const newUser = await authService.register(data);
+            res.status(201).json({
+                message:"Created User successfully",
+                statusCode:201,
+                data:[newUser]
+            })
+        } catch(error:any){
+            res.status(500).json({
+                message:"Server Internal error ",
+                error:error.message,
+                statusCode:500,
+            })
+        }
+    },
+}
