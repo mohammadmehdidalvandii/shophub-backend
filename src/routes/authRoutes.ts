07519835ef67a547ab5a74ea182cf4a0ec175f8e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-15 11:16:07 +0330

    created validationRegister successfully

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authController } from "../controller/auth.controller";
 import { authenticateToken } from "../middleware/authenticateToken";
+import { validationRegister } from "../validation/Validation.user";
 
 const router = Router();
 
-router.post('/register', authController.register);
+router.post('/register', validationRegister ,authController.register);
 router.post('/login', authController.login);
 router.post('/refresh-token', authController.refreshToken);
 router.post('/logout', authController.logout);
---
+++ src/validation/Validation.user.ts
@@ -0,0 +1,29 @@
+import {body , validationResult} from 'express-validator';
+import { next, req, res } from '../types/express';
+
+const validationRegister = [
+    body('firstName').isLength({min:3}).withMessage('The FirstName must be 3 characters or more').notEmpty().withMessage('Felid is required'),
+    body('lastName').isLength({min:3}).withMessage('The LastName must be 3 characters or more').notEmpty().withMessage('Felid is required'),
+    body('email').isEmail().withMessage('Email is not valid').notEmpty().withMessage('Email is not valid'),
+    body('password')
+    .notEmpty().withMessage('Password is required')
+    .isLength({min:8}).withMessage('Password must be at least 8 characters long')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/[a-z]/).withMessage('password must contain at least one lowercase letter')
+    .matches(/[\d]/).withMessage('Password must contain at least one number')
+    .matches(/[@$!%*#/"]/).withMessage('Password must contain at least one special characters'), 
+    body('phone').notEmpty().withMessage('Phone is required'),
+    (req:req , res:res ,next:next)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({error:errors.array()})
+        }
+        next();
+    }
+];
+
+
+
+export {
+    validationRegister
+}
