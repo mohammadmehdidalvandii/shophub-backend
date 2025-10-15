fec73d09ebed30865cf0846528209c697fd2ea19
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-15 14:11:43 +0330

    created validation Login successfully

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { authController } from "../controller/auth.controller";
 import { authenticateToken } from "../middleware/authenticateToken";
-import { validationRegister } from "../validation/Validation.user";
+import { validationLogin, validationRegister } from "../validation/Validation.user";
 
 const router = Router();
 
 router.post('/register', validationRegister ,authController.register);
-router.post('/login', authController.login);
+router.post('/login', validationLogin ,authController.login);
 router.post('/refresh-token', authController.refreshToken);
 router.post('/logout', authController.logout);
 router.get('/profile', authenticateToken ,authController.profile);
--- src/validation/Validation.user.ts
+++ src/validation/Validation.user.ts
@@ -22,8 +22,26 @@ const validationRegister = [
     }
 ];
 
+const validationLogin = [
+    body('email').isEmail().withMessage('Email is not valid').notEmpty().withMessage('Email is not valid'),
+    body('password')
+    .notEmpty().withMessage('Password is required')
+    .isLength({min:8}).withMessage('Password must be at least 8 characters long')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/[a-z]/).withMessage('password must contain at least one lowercase letter')
+    .matches(/[\d]/).withMessage('Password must contain at least one number')
+    .matches(/[@$!%*#/"]/).withMessage('Password must contain at least one special characters'),
+    (req:req ,res:res , next:next) =>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({error:errors.array()})
+        }
+        next()
+    }
+]
 
 
 export {
-    validationRegister
+    validationRegister,
+    validationLogin
 }
