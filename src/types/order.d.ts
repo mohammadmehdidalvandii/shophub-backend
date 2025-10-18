2d9dc2021f7e1414494ddafa661c2e64ae5c84f2
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-18 11:20:48 +0330

    created orderProps

---
+++ src/types/order.d.ts
@@ -0,0 +1,23 @@
+export type orderProps={
+    user:string,
+    items:[
+        product:string,
+        quantity:number,
+        price:number,
+    ],
+    customerInfo:{
+        firstName:string,
+        lastName:string,
+        email:string,
+        phone:string,
+        address:string,
+        city:string,
+        state:string,
+        postalCode:string,
+        country:string,
+        orderNote :string,
+    },
+    totalAmount:string,
+    status:'pending'|'paid'|'shipped'|'completed'|'cancelled',
+    paymentMethod:string,
+},
