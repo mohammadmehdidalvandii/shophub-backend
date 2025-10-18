b2b1fac0eb99d55235255a3286c580d5b62a15d3
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-10-18 09:13:44 +0330

    add only admin create delete patch for product

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { productController } from "../controller/product.controller";
 import upload from "../config/multer";
+import { authenticateToken } from "../middleware/authenticateToken";
+import { authorizeAdmin } from "../middleware/authorizeAdmin";
+
 const router = Router();
 
 router.get('/', productController.getAll);
 router.get('/:id', productController.getProductID);
-router.post('/create', upload.array('images',5), productController.create);
-router.patch('/:id' , upload.array('images',5) ,  productController.update);
-router.delete('/:id',productController.delete)
+router.post('/create',authenticateToken ,authorizeAdmin ,upload.array('images',5), productController.create);
+router.patch('/:id',authenticateToken , authorizeAdmin , upload.array('images',5) ,  productController.update);
+router.delete('/:id', authenticateToken , authorizeAdmin,productController.delete)
 
 export default router;
