5e002c6f3bde4348a209c855fe821d62e0b29a80
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-11-16 10:27:38 +0330

    created logic delete messages:

--- src/controller/message.controller.ts
+++ src/controller/message.controller.ts
@@ -26,5 +26,44 @@ export const messageController = {
                 error:error.message,
             })
         }
+    },
+    async getAll(req:req , res:res){
+        try{
+            const messages = await messageServices.getAllMessage();
+            res.status(200).json({
+                message:"Get all messages successfully",
+                statusCode:200,
+                data:messages
+            })
+        }catch(error:any){
+            return res.status(500).json({
+                message:"Server Internal Error getAllMessage",
+                statusCode:500,
+                error:error.message
+            })
+        }
+    },
+    async delete(req:req, res:res){
+        try{
+            const {id} = req.params;
+            if(!id){
+                return res.status(400).json({
+                    message:"Id is not found",
+                    statusCode:400,
+                })
+            };
+            const message = await messageServices.deleteMessage(id);
+            res.status(200).json({
+                message:'delete message successfully',
+                statusCode:200,
+                data:message
+            })
+        }catch(error:any){
+            return res.status(500).json({
+                message:"Server Internal Error Delete message",
+                statusCode:500,
+                error:error.message,
+            })
+        }
     }
 }
--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -3,6 +3,8 @@ import { messageController } from "../controller/message.controller";
 
 const router = Router();
 
+router.get('/', messageController.getAll)
 router.post('/create', messageController.create);
+router.delete('/delete/:id', messageController.delete);
 
 export default router
